ed6039da9f3b4a1c93055c35932a087dff7f53d7
Raul Colina <rcolina@gmail.com>
2021-10-04 20:24:04 -0700

    Cadenas de python

---
+++ origin.py
@@ -0,0 +1,16 @@
+name='raul colina castillon'
+age=10
+istall=True
+height=1.75
+
+print (name+" tiene "+str(age)+" años")
+print("{} tiene {} años".format(name,age))
+
+print(f"{name} tiene {age+10} años y es \
+alto {istall} y mide {height}")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print (name*5)
